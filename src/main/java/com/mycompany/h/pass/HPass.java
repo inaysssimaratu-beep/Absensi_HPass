47211edfe612619e4aa074c0bfa729cd2f6361ef
tephen229 <dwiriski2209@gmail.com>
2026-04-19 11:50:03 +0700

    First Commit

---
+++ src/main/java/com/mycompany/h/pass/HPass.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.h.pass;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class HPass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
