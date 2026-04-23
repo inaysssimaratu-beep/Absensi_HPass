2e91e51013ee37e283feac7f0f31ffaeb6085160
tephen229 <dwiriski2209@gmail.com>
2026-04-23 07:59:41 +0700

    First Commit

--- src/main/java/com/mycompany/h/pass/MainApp.java
+++ src/main/java/com/mycompany/h/pass/MainApp.java
@@ -1,18 +1,28 @@
 package com.mycompany.h.pass;
 
 import com.mycompany.h.pass.objects.Penghuni;
+import com.mycompany.h.pass.objects.GenericDAO;
 
 public class MainApp {
     public static void main(String[] args) {
+        // 1. Inisialisasi GenericDAO khusus untuk objek Penghuni
+        GenericDAO<Penghuni> penghuniDAO = new GenericDAO<>("penghuni", Penghuni.class);
+
+        // 2. Membuat data penghuni (Contoh data kamu)
         Penghuni p = new Penghuni();
         p.setUidRfid("87654321");
         p.setNim("24090028");
         p.setNama("Dwi Riski Ariyanto");
-        p.setKamar("Asrama-01");
+        p.setKamar("A-01");
         p.setFakultas("Teknik");
 
-        // Print hasil test
-        System.out.println("Data Berhasil Dibuat:");
-        System.out.println(p.toString());
+        // 3. Simpan data
+        penghuniDAO.save(p);
+
+        // 4. Tampilkan data yang tersimpan
+        System.out.println("\n--- Daftar Penghuni di Memori ---");
+        for (Penghuni data : penghuniDAO.findAll()) {
+            System.out.println("NIM: " + data.getNim() + " | Nama: " + data.getNama());
+        }
     }
 }
---
+++ src/main/java/com/mycompany/h/pass/objects/BaseDAO.java
@@ -0,0 +1,11 @@
+package com.mycompany.h.pass.objects;
+
+import java.util.List;
+
+public interface BaseDAO<T> {
+    void save(T entity);
+    void update(int index, T entity);
+    void delete(int index);
+    List<T> findAll();
+    T findByIndex(int index);    
+}
